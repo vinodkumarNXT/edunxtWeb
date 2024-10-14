09fa65461b8ebb74a4420c50e39b9b09ccf9f1ce
VINODKUMARCVK\chvin <chvinodkumarvinnu@gmail.com>
2024-10-14 15:27:30 +0530

    intial

---
+++ projects/edu-demo/webpack.config.js
@@ -0,0 +1,57 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "eduDemo",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        library: { type: "module" },
+
+        // For remotes (please adjust)
+        // name: "eduDemo",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './projects/edu-demo/src/app/app.component.ts',
+        // },
+
+        // For hosts (please adjust)
+        // remotes: {
+        //     "shell": "http://localhost:4200/remoteEntry.js",
+        //     "edunxt": "http://localhost:5001/remoteEntry.js",
+
+        // },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
